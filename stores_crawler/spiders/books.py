8c3ea5a9c4f3a345f6779ec94e37aeb17ce609ec
gagan goyal <goyalgagan82@gmail.com>
2023-04-24 14:54:37 +0530

    use Selenium to scrap all pages

--- stores_crawler/spiders/books.py
+++ stores_crawler/spiders/books.py
@@ -1,13 +1,39 @@
-from scrapy.spiders import CrawlSpider, Rule
-from scrapy.linkextractors import LinkExtractor
+from time import sleep
+from scrapy import Spider
+from selenium import webdriver
+from scrapy.selector import Selector
+from scrapy.http import Request
+from selenium.common.exceptions import NoSuchElementException
 
-
-class BooksSpider(CrawlSpider):
+class BooksSpider(Spider):
     name = "books"
     allowed_domains = ["books.toscrape.com"]
-    start_urls = ["http://books.toscrape.com/"]
 
-    rules = (Rule(LinkExtractor(deny_domains=('google.com'),allow=('music')), callback='parse_page', follow=False),)
+    def start_requests(self):
+        self.driver = webdriver.Chrome('/opt/homebrew/bin/chromedriver')
+        self.driver.get('http://books.toscrape.com')
+        sel = Selector(text=self.driver.page_source)
+        books = sel.xpath('//h3/a/@href').extract()
+        for book in books:
+            url = 'http://books.toscrape.com/' + book
+            yield Request(url, callback=self.parse_book)
 
-    def parse_page(self, response):
+        while True:
+            try:
+                next_page = self.driver.find_element("xpath",'//a[text()="next"]')
+                sleep(3)
+                self.logger.info('Sleeping for 3 seconds')
+                next_page.click()
+
+                sel = Selector(text=self.driver.page_source)
+                books = sel.xpath('//h3/a/@href').extract()
+                for book in books:
+                    url = 'http://books.toscrape.com/catalogue/' + book
+                    yield Request(url, callback=self.parse_book)
+            except NoSuchElementException:
+                self.logger.info('No more pages to load')
+                self.driver.quit()
+                break
+
+    def parse_book(self, response):
         pass
